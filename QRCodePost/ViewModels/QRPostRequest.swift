6d170daada43069c0ba270ac6820f3666c5b0452
GenuineMas <MasalitinJW@gmail.com>
2021-08-05 17:09:20 +0300

    adding post method with decoding qr code result

---
+++ QRCodePost/ViewModels/QRPostRequest.swift
@@ -0,0 +1,37 @@
+//
+//  QRPostRequest.swift
+//  QRCodePost
+//
+//  Created by Genuine on 05.08.2021.
+//
+
+
+import Alamofire
+import Combine
+import Foundation
+
+@available(iOS 13.0, *)
+
+
+class QRViewModel: ObservableObject {
+    
+    @Published  var QRCodeResult: String?
+    var result = ResponseServer()
+    
+    
+    func postCode(code: String)  {
+        let url = URL(string: "https://httpbin.org/post")!
+        AF.request(url, method: .post, parameters: ["QR": code], encoder: JSONParameterEncoder.default)
+            .validate()
+            .responseJSON { [weak self] response in
+            let decoder = JSONDecoder()
+            if let jsonQRCode = try? decoder.decode(QRCode.self, from: response.data!) {
+                
+                resultQR.qrResult = jsonQRCode.json.values.first
+                self!.QRCodeResult = jsonQRCode.json.values.first!
+                print ("QRCodeResult(postcode) : ", self!.QRCodeResult)
+                
+            }
+        }
+    }
+}
