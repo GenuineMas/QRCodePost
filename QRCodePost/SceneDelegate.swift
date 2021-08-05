5a3ed1e8798070d505d383ef013ac38f610b5668
GenuineMas <MasalitinJW@gmail.com>
2021-08-05 17:40:12 +0300

    change settings for root view controller

--- QRCodePost/SceneDelegate.swift
+++ QRCodePost/SceneDelegate.swift
@@ -13,6 +13,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+//        self.window = UIWindow(frame: UIScreen.main.bounds)
+//        let nav = UINavigationController(rootViewController: ScannerViewController())
+//        self.window?.rootViewController = nav
+//        self.window?.makeKeyAndVisible()
+//        
+        if let windowScene = scene as? UIWindowScene {
+
+                    let window = UIWindow(windowScene: windowScene)
+                    let scanner = ScannerViewController()
+
+                    let navigation = UINavigationController(rootViewController: scanner)
+                    window.rootViewController = navigation
+
+                    self.window = window
+                    window.makeKeyAndVisible()
+                }
+
+
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
